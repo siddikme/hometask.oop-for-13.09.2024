6ddcb16a6d8fda7266bfcee2bc238d8a50ac9547
Your Name <lala8008484@gmail.com>
2024-09-13 18:16:11 +0500

    hometaskfor13.09.2024

---
+++ main.py
@@ -0,0 +1,57 @@
+# taslk1
+# class Person:
+#     def __init__ (self,name):
+#         self.name = name
+#     def greet (self):
+#             print("Hello , my name is ",self.name)
+# obj1 = Person("Abubakr")
+# obj1.greet()
+# solved:
+
+# task2
+# class Rectangle:
+#     def __init__  (self,length,width):
+#         self.length = length
+#         self.width = width  
+
+#     def area (self):
+#         return self.length * self.width
+    
+# a = Rectangle(5,6)
+# print(a.area())
+# not solved:
+
+# task3
+# class Circle:
+#     pi = 3.14
+
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     def area(self):
+#         return Circle.pi * self.radius ** 2
+
+# circle = Circle(2) 
+# print(circle.area())
+# solved:
+
+# task4
+
+# class Car:
+#     total_cars = 0
+
+#     def __init__ (self):
+#         Car.total_cars += 1
+
+#     @classmethod
+#     def total_car_count(cls):
+#         return cls.total_cars
+
+# car1 = Car()
+# car2 = Car()
+# car3 = Car()
+# car4 = Car()
+
+# print(Car.total_car_count())
+# solved:
+
